06f421a60cd10ff6084c5dc049871b2171166850
Hao Chen <mmkrty@gmail.com>
2023-07-13 15:49:12 +0200

    setup filter component and add to home

--- src/app/pages/home/components/filters/filters.component.ts
+++ src/app/pages/home/components/filters/filters.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-filters',
   templateUrl: './filters.component.html',
-  styleUrls: ['./filters.component.css']
+  styleUrls: ['./filters.component.css'],
 })
 export class FiltersComponent implements OnInit {
+  categories = ['shoes', 'sports'];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
