0a991c88a1cf75787fa6bcf57edf7004e656afd0
Hao Chen <mmkrty@gmail.com>
2023-07-13 14:51:00 +0200

    emit cols number to home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  cols = 3;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onColumnsChange(colsNumber: number): void {
+    this.cols = colsNumber;
   }
-
 }
