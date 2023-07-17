ad98e3bf70c1f7a9e05c8d23151c1504535dc352
Hao Chen <mmkrty@gmail.com>
2023-07-17 11:13:04 +0200

    setup grid in home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+const ROWS_HEIGHT: { [id: number]: number } = {
+  1: 400,
+  3: 335,
+  4: 350,
+};
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,6 +13,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   cols = 3;
+  rowHeight = ROWS_HEIGHT[this.cols];
   category: string | undefined;
 
   constructor() {}
