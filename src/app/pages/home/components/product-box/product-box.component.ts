0d85547e2e99d6792a19b8bff9aef79f7ce75ed7
Hao Chen <mmkrty@gmail.com>
2023-07-17 14:15:56 +0200

    implement single full width view for product box

--- src/app/pages/home/components/product-box/product-box.component.ts
+++ src/app/pages/home/components/product-box/product-box.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product-box',
   templateUrl: './product-box.component.html',
-  styleUrls: ['./product-box.component.css']
+  styleUrls: ['./product-box.component.css'],
 })
 export class ProductBoxComponent implements OnInit {
+  @Input() fullWidthMode = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
