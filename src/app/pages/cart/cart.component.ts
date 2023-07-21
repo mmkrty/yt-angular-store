45c5c13b5588385034515eafd114bfa1d489f00c
Hao Chen <mmkrty@gmail.com>
2023-07-21 09:26:41 +0200

    initialize cart component and models

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Cart, CartItem } from 'src/app/models/cart.model';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent implements OnInit {
+  cart: Cart = {
+    items: [
+      {
+        product: 'https://via.placeholder.com/150',
+        name: 'sneakers',
+        price: 150,
+        quantity: 1,
+        id: 1,
+      },
+    ],
+  };
+  dataSource: CartItem[] = [];
+  displayedColumns: string[] = ['product', 'name', 'price', 'quantity', 'total', 'action'];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.dataSource = this.cart.items;
   }
-
 }
