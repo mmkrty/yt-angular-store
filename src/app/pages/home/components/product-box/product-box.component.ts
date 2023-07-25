c22aa2199673302ee6626b126e991edd3cea0323
Hao Chen <mmkrty@gmail.com>
2023-07-25 15:13:56 +0200

    initialize product model

--- src/app/pages/home/components/product-box/product-box.component.ts
+++ src/app/pages/home/components/product-box/product-box.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
 
 @Component({
   selector: 'app-product-box',
@@ -7,8 +8,18 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ProductBoxComponent implements OnInit {
   @Input() fullWidthMode = false;
+  product: Product = {
+    id: 1,
+    title: 'sneakers',
+    price: 150,
+    category: 'sneakers',
+    description: 'sneakers',
+    image: 'https://via.placeholder.com/150',
+  };
 
   constructor() {}
 
   ngOnInit() {}
+
+  onAddToCart() {}
 }
