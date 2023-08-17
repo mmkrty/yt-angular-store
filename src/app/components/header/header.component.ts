2292cab98ed5a12b39acf7bc17afcec28ce52bf8
Hao Chen <mmkrty@gmail.com>
2023-08-17 16:03:55 +0200

    initialize cart getter and setter in header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Cart } from 'src/app/models/cart.model';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
+  private _cart: Cart = {items: []};
+  itemsQuantity = 0;
+
+  @Input()
+  get cart(): Cart {
+    return this._cart;
+  } 
+
+  set cart(cart: Cart) {
+    this._cart = cart;
+    this.itemsQuantity = cart.items
+      .map((items) => items.quantity)
+      .reduce((prev, current) => prev + current, 0);
+  }
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
